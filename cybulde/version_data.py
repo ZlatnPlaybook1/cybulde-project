b176e96714df1cbd90e04f0403622a6a8a5e4f3f
Ahmed Sherif <as4349124@gmail.com>
2025-09-02 11:31:16 +0300

    added functionality to initialize DVC

---
+++ cybulde/version_data.py
@@ -0,0 +1,13 @@
+from cybulde.config_schemas.config_schema import Config
+from cybulde.utils.config_utils import get_config
+from cybulde.utils.utils import get_logger 
+from cybulde.utils.data_utils import initialize_dvc
+from pathlib import Path
+
+@get_config(config_path="../configs", config_name="config")
+def version_data(config: Config) -> None:
+    initialize_dvc()
+
+
+if __name__ == "__main__":
+    version_data()  # type: ignore
